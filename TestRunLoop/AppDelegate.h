13fc0d16452c18efbdae5a1eb808b6aee97ad833
范金鑫 <jinxin.fan@renren-inc.com>
2017-06-14 19:39:51 +0800

    Initial Commit

---
+++ TestRunLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRunLoop
+//
+//  Created by Jinxin_Fan on 2017/6/14.
+//  Copyright © 2017年 Jinxin_Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRunLoop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRunLoop
+//
+//  Created by Jinxin_Fan on 2017/6/14.
+//  Copyright © 2017年 Jinxin_Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
